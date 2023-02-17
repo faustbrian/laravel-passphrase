d28dbc31147098ce86b0e281ae51c37c5a30ccef
Brian Faust <hello@basecode.sh>
2023-02-17 07:30:43 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ tests/Unit/EFFTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use PreemStudio\Passphrase\EFF;
+
+it('should generate a random passphrase with the five dice list', function (): void {
+    expect(EFF::useFiveDiceList()->generate(5))->toBeString();
+});
+
+it('should generate a random passphrase with the four dice list', function (): void {
+    expect(EFF::useFourDiceList()->generate(5))->toBeString();
+});
+
+it('should generate a random passphrase with the unique three character list', function (): void {
+    expect(EFF::useUniqueThreeCharacterList()->generate(5))->toBeString();
+});
+
+it('should generate a random passphrase with a custom separator', function (): void {
+    expect(EFF::useFiveDiceList()->useSeperator('///')->generate(5))->toContain('///');
+});
