ea0ba7edf86f27fe43da657b218d07011660361b
Brian Faust <hello@basecode.sh>
2023-03-13 13:05:45 +0200

    chore: apply global pint configuration

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,16 +7,10 @@ namespace PreemStudio\Passphrase;
 use Illuminate\Contracts\Container\Container;
 use League\CommonMark\Environment\Environment;
 use PreemStudio\Jetpack\Package\AbstractServiceProvider;
-use PreemStudio\Jetpack\Package\Package;
 use PreemStudio\Passphrase\Contracts\Generator;
 
 final class ServiceProvider extends AbstractServiceProvider
 {
-    public function configurePackage(Package $package): void
-    {
-        $package->name('laravel-passphrase')->hasConfigFile();
-    }
-
     public function packageRegistered(): void
     {
         $this->app->singleton('passphrase.generator', function (Container $app): Environment {
